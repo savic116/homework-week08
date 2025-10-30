44158bfff68a2e3da12ed82a0633615c2e9cabe7
savic116 <savic00085@gmail.com>
2025-10-30 13:28:26 +0900

    Updated Changes

---
+++ lib/week08/calendar_scheduler/model/schedule_model.dart
@@ -0,0 +1,11 @@
+class ScheduleModel{
+  final String id;
+  final String content;
+  final DateTime date;
+  final int startTime;
+  final int endTime;
+
+  ScheduleModel({
+    required this.id,
+  })
+}
